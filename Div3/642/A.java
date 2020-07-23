8f827e4e8f66c1176c7dfc30b2cb14ddd9f1fd58
Jagrit <44747746+Jagrit29@users.noreply.github.com>
2020-07-23 21:19:58 +0530

    Create A.java

---
+++ Div3/642/A.java
@@ -0,0 +1,102 @@
+package Div3_642;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class A {
+	static class FastReader 
+    { 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader() 
+        { 
+            br = new BufferedReader(new
+                     InputStreamReader(System.in)); 
+        } 
+  
+        String next() 
+        { 
+            while (st == null || !st.hasMoreElements()) 
+            { 
+                try
+                { 
+                    st = new StringTokenizer(br.readLine()); 
+                } 
+                catch (IOException  e) 
+                { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        int nextInt() 
+        { 
+            return Integer.parseInt(next()); 
+        } 
+  
+        long nextLong() 
+        { 
+            return Long.parseLong(next()); 
+        } 
+  
+        double nextDouble() 
+        { 
+            return Double.parseDouble(next()); 
+        } 
+  
+        String nextLine() 
+        { 
+            String str = ""; 
+            try
+            { 
+                str = br.readLine(); 
+            } 
+            catch (IOException e) 
+            { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+        } 
+        int[] nextIntArray(int n)
+        {
+        	int a[]=new int[n];
+        	for(int i=0;i<n;i++)
+        	{
+        		a[i]=Integer.parseInt(next()); 
+        	}
+        	return a;
+        }
+        
+    } 
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader sc=new FastReader();
+		int t=sc.nextInt();
+		while(t-- > 0)
+		{
+			int n=sc.nextInt();
+			long m=sc.nextInt();
+			if(n==1)
+			{
+				System.out.println(0);
+			}
+			else if(n==2)
+			{
+				System.out.println(m);
+			}
+			else
+			{
+				//[0,m,0] for 3;
+				System.out.println(m*2);
+			}
+		}
+		
+
+	}
+
+}

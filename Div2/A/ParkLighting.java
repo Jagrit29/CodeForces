3ad586c1397fcaf451e35bfef5952e754ff45e55
Jagrit <44747746+Jagrit29@users.noreply.github.com>
2020-09-16 03:13:18 +0530

    Create ParkLighting.java

---
+++ Div2/A/ParkLighting.java
@@ -0,0 +1,83 @@
+
+import java.util.*;
+import java.math.*;
+import java.io.*;
+import java.lang.*;
+public class Main {
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+ 
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+ 
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+ 
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+ 
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+ 
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+ 
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+ 
+		int[] nextIntArray(int n) {
+			int a[] = new int[n];
+			for (int i = 0; i < n; i++) {
+				a[i] = Integer.parseInt(next());
+			}
+			return a;
+		}
+ 
+	}
+	
+	private static long startTime = System.currentTimeMillis();
+	public static void main(String[] args) {
+		FastReader sc =new FastReader();
+		int t=sc.nextInt();
+		while(t-- > 0)
+		{
+			int n=sc.nextInt();
+			int m=sc.nextInt();
+			int count=n;
+			count=count*(m/2); 
+			if(m%2!=0)
+			{
+				count=count+((n+1)/2);
+			}
+			System.out.println(count);
+			
+			
+			//long endTime = System.currentTimeMillis();
+		    //System.out.println("It took " + (endTime - startTime) + " MILIseconds");
+		    
+			
+		}
+	}
+	
+ 
+}

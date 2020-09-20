226471df9d1b3a3078396ec11246047a904fb309
Jagrit <44747746+Jagrit29@users.noreply.github.com>
2020-09-20 23:40:02 +0530

    Create 991A.java

---
+++ Div2/A/991A.java
@@ -0,0 +1,80 @@
+
+import java.util.*;
+import java.math.*;
+import java.io.*;
+import java.lang.*;
+public class Main {
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+ 
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+ 
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+ 
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+ 
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+ 
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+ 
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+ 
+		int[] nextIntArray(int n) {
+			int a[] = new int[n];
+			for (int i = 0; i < n; i++) {
+				a[i] = Integer.parseInt(next());
+			}
+			return a;
+		}
+ 
+	}
+	
+	private static long startTime = System.currentTimeMillis();
+	public static void main(String[] args) {
+		FastReader sc =new FastReader();
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		int t=sc.nextInt();
+		
+		a=a-c; //only a;
+		b=b-c; //only b;
+		
+		int left=t-a-b-c;
+		if(a>=0 && b>=0 && c>=0 && left>0)
+		{
+			System.out.println(left);
+		}
+		else{
+			System.out.println(-1);
+		}
+
+        
+	}
+}

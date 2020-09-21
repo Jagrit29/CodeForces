2b0daea6883a593b77492cbf6e9a5a41042145cd
Jagrit <44747746+Jagrit29@users.noreply.github.com>
2020-09-21 10:52:16 +0530

    Create LinksAndPearls.java

---
+++ Div2/A/LinksAndPearls.java
@@ -0,0 +1,90 @@
+
+import java.util.*;
+import java.math.*;
+import java.beans.beancontext.BeanContextServiceAvailableEvent;
+import java.io.*;
+import java.lang.*;
+public class Main {
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+ 
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+ 
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+ 
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+ 
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+ 
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+ 
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+ 
+		int[] nextIntArray(int n) {
+			int a[] = new int[n];
+			for (int i = 0; i < n; i++) {
+				a[i] = Integer.parseInt(next());
+			}
+			return a;
+		}
+ 
+	}
+	
+	private static long startTime = System.currentTimeMillis();
+	public static void main(String[] args) {
+		FastReader sc =new FastReader();
+		String s=sc.next();
+		int link=0;
+		int pearl=0;
+		for(int i=0;i<s.length();i++)
+		{
+			char ch=s.charAt(i);
+			if(ch=='o')
+			{
+				pearl++;
+			}
+			else{
+				link++;
+			}
+		}
+		if(pearl==0)
+		{
+			System.out.println("YES");
+			return;
+		}
+		if(link%pearl==0)
+		{
+			System.out.println("YES");
+		}
+		else{
+			System.out.println("NO");
+		}
+        
+	}
+}

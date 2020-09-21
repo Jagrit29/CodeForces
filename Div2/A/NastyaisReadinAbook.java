fd208ff652453b8a1de040729a7cb38b1644e9ac
Jagrit <44747746+Jagrit29@users.noreply.github.com>
2020-09-21 22:52:43 +0530

    Create NastyaisReadinAbook.java

---
+++ Div2/A/NastyaisReadinAbook.java
@@ -0,0 +1,83 @@
+
+import java.util.*;
+import java.math.*;
+import java.beans.beancontext.BeanContextServiceAvailableEvent;
+import java.io.*;
+import java.lang.*;
+public class Main {
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+ 
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+ 
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+ 
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+ 
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+ 
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+ 
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+ 
+		int[] nextIntArray(int n) {
+			int a[] = new int[n];
+			for (int i = 0; i < n; i++) {
+				a[i] = Integer.parseInt(next());
+			}
+			return a;
+		}
+ 
+	}
+	
+	private static long startTime = System.currentTimeMillis();
+	public static void main(String[] args) {
+		FastReader sc =new FastReader();
+		int n=sc.nextInt();
+		int a[][]=new int[n][2];
+		for(int i=0;i<n;i++)
+		{
+			a[i][0]=sc.nextInt();
+			a[i][1]=sc.nextInt();
+		}
+		int k=sc.nextInt();
+		int cut=0;
+		for(int i=0;i<n;i++)
+		{
+			if(k<=a[i][1])
+			{
+				cut=i;
+				break;
+			}
+		}
+		System.out.println(n-cut);
+		
+		
+	}
+}

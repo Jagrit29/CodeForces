578e482b7adfa26046b64b45bacf864fe8c858eb
Jagrit <44747746+Jagrit29@users.noreply.github.com>
2020-09-24 11:42:29 +0530

    Create RomanAndBrowser.java

---
+++ Div2/A/RomanAndBrowser.java
@@ -0,0 +1,85 @@
+
+import java.util.*;
+import java.math.*;
+import java.beans.beancontext.BeanContextServiceAvailableEvent;
+import java.io.*;
+import java.lang.*;
+public class Main {
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+ 
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+ 
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+ 
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+ 
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+ 
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+ 
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+ 
+		int[] nextIntArray(int n) {
+			int a[] = new int[n];
+			for (int i = 0; i < n; i++) {
+				a[i] = Integer.parseInt(next());
+			}
+			return a;
+		}
+ 
+	}
+	
+	private static long startTime = System.currentTimeMillis();
+	public static void main(String[] args) {
+		FastReader sc =new FastReader();
+		int n=sc.nextInt();
+		int k=sc.nextInt();
+		int a[]=sc.nextIntArray(n);
+		int ans=0;
+		//I can try all values of b from 0 to n;
+		for(int i=0;i<k;i++)
+		{
+			int curr=0;
+			for(int j=0;j<n;j++)
+			{
+				if(j%k==i)
+				{
+
+				}
+				else{
+					curr=curr+a[j];
+				}
+			}
+			ans=Math.max(ans,Math.abs(curr));
+		}
+		System.out.println(ans);
+		
+	}
+}

b898627e16579565f9a26e21c23182ec69be1951
Jagrit <44747746+Jagrit29@users.noreply.github.com>
2020-09-25 13:16:17 +0530

    Create RopeWalkers.java

---
+++ Div2/A/RopeWalkers.java
@@ -0,0 +1,82 @@
+
+import java.util.*;
+import java.math.*;
+import java.beans.beancontext.BeanContextServiceAvailableEvent;
+import java.io.*;
+import java.lang.*;
+public class Main {
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+ 
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+ 
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+ 
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+ 
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+ 
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+ 
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+ 
+		int[] nextIntArray(int n) {
+			int a[] = new int[n];
+			for (int i = 0; i < n; i++) {
+				a[i] = Integer.parseInt(next());
+			}
+			return a;
+		}
+ 
+	}
+	
+	private static long startTime = System.currentTimeMillis();
+	public static void main(String[] args) {
+		FastReader sc =new FastReader();
+		long a[]=new long[3];
+		for(int i=0;i<3;i++)
+		{
+			a[i]=sc.nextLong();
+		}
+		long d=sc.nextInt();
+		Arrays.sort(a);
+		long count=0;
+		long diff1=a[1]-a[0];
+		if(diff1<d)
+		{
+			count=count+(d-diff1);
+		}
+		long diff2=a[2]-a[1];
+		if(diff2<d)
+		{
+			count=count+(d-diff2);
+		}
+		System.out.println(count);
+	}
+}

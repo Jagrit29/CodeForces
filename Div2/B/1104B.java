e25ab6cd47ae829e6de56682f2132c9e6bc22f63
Jagrit <44747746+Jagrit29@users.noreply.github.com>
2020-09-26 09:06:22 +0530

    Create 1104B.java

---
+++ Div2/B/1104B.java
@@ -0,0 +1,87 @@
+
+import java.util.*;
+import java.math.*;
+import java.beans.beancontext.BeanContextServiceAvailableEvent;
+import java.io.*;
+import java.lang.*;
+public class Main {
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+ 
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+ 
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+ 
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+ 
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+ 
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+ 
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+ 
+		int[] nextIntArray(int n) {
+			int a[] = new int[n];
+			for (int i = 0; i < n; i++) {
+				a[i] = Integer.parseInt(next());
+			}
+			return a;
+		}
+ 
+	}
+	
+	private static long startTime = System.currentTimeMillis();
+	public static void main(String[] args) {
+		FastReader sc =new FastReader();
+		String s=sc.next();
+		Stack<Character> stack=new Stack<>();
+		boolean win=false;
+		for(int i=0;i<s.length();i++)
+		{
+			char ch=s.charAt(i);
+			if(stack.size()==0)
+			{
+				stack.push(ch);
+			}
+			else{
+				if(stack.peek()==ch)
+				{
+					stack.pop();
+					win=!win;
+				}
+				else{
+					stack.push(ch);
+				}
+			}
+		}
+		System.out.println(win ? "Yes" : "No");
+
+
+	}
+}

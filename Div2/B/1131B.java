188ab0fae73a627187638b22fe7f079729d50fe1
Jagrit <44747746+Jagrit29@users.noreply.github.com>
2020-09-27 00:09:30 +0530

    Create 1131B.java

---
+++ Div2/B/1131B.java
@@ -0,0 +1,121 @@
+
+import java.util.*;
+import java.math.*;
+import java.beans.beancontext.BeanContextServiceAvailableEvent;
+import java.io.*;
+import java.lang.*;
+public class Main {
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+ 
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+ 
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+ 
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+ 
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+ 
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+ 
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+ 
+		int[] nextIntArray(int n) {
+			int a[] = new int[n];
+			for (int i = 0; i < n; i++) {
+				a[i] = Integer.parseInt(next());
+			}
+			return a;
+		}
+ 
+	}
+	
+	private static long startTime = System.currentTimeMillis();
+	public static void main(String[] args) {
+		FastReader sc =new FastReader();
+		int n = sc.nextInt();
+		int start1=sc.nextInt();
+		int start2=sc.nextInt();
+		int count=1+Math.min(start1,start2);
+		for(int i=1;i<n;i++)
+		{
+			int end1=sc.nextInt();
+			int end2=sc.nextInt();
+			if(start1==end1 && start2==end2) continue;
+
+			int start=Math.max(start1,start2);
+			int end=Math.min(end1,end2);
+
+			if(end>=start)
+			{
+				count+=end-start+1;
+			}
+			if(start1==start2) count--;
+			start1=end1;
+			start2=end2;
+		}
+		System.out.println(count);
+	}
+
+}
+
+//Templates for Comparator and Classes - @jagrit_07
+/*
+class Pair
+	{
+		
+		long i; //index;
+		long l; //left;
+		long c; //cost;
+		public Pair(long x,long y,long z)
+		{
+			this.i=x;
+			this.l=y;
+			this.c=z;
+		}
+		public String toString()
+		{
+			return this.i+" "+this.l+" "+this.c;
+		}
+	}
+class Comp implements Comparator<Pair>
+	{
+		public int compare(Pair p1, Pair p2)
+		{
+			if(p1.c!=p2.c)
+			{
+				return (int)(p1.c-p2.c); //sort acc to cost;
+			}
+			else{
+				return (int)(p1.i-p2.i); //sort acc to index;
+			}
+			
+		}
+	}
+*/

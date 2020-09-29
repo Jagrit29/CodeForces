6ed8cb0a7ff1d39bd921a8e9eded7b7314b43df9
Jagrit <44747746+Jagrit29@users.noreply.github.com>
2020-09-29 07:04:09 +0530

    Create 1139B.java

---
+++ Div2/B/1139B.java
@@ -0,0 +1,114 @@
+
+import java.util.*;
+import java.math.*;
+import java.io.*;
+import java.lang.*;
+public class Main {
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+ 
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+ 
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (final IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (final IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+		int[] nextIntArray(final int n) {
+			final int a[] = new int[n];
+			for (int i = 0; i < n; i++) {
+				a[i] = Integer.parseInt(next());
+			}
+			return a;
+		}
+
+	}
+
+	private static long startTime = System.currentTimeMillis();
+
+	public static void main(final String[] args) {
+		FastReader sc =new FastReader();
+		int n=sc.nextInt();
+		int a[]=sc.nextIntArray(n);
+		long ans=a[n-1];
+		int lastBought=a[n-1];
+		for(int i=n-2;i>=0;i--)
+		{
+			if(lastBought<=1) break;
+			ans=ans+Math.max(0,Math.min(a[i],lastBought-1));
+			//System.out.println(ans);
+			lastBought=Math.min(a[i],lastBought-1);
+		}
+		System.out.println(ans);
+	}
+}
+
+//Templates for Comparator and Classes - @jagrit_07
+/*
+class Pair
+	{
+		
+		long i; //index;
+		long l; //left;
+		long c; //cost;
+		public Pair(long x,long y,long z)
+		{
+			this.i=x;
+			this.l=y;
+			this.c=z;
+		}
+		public String toString()
+		{
+			return this.i+" "+this.l+" "+this.c;
+		}
+	}
+class Comp implements Comparator<Pair>
+	{
+		public int compare(Pair p1, Pair p2)
+		{
+			if(p1.c!=p2.c)
+			{
+				return (int)(p1.c-p2.c); //sort acc to cost;
+			}
+			else{
+				return (int)(p1.i-p2.i); //sort acc to index;
+			}
+			
+		}
+	}
+*/
+
+/*
+  HashMap - Put template - d.put(a1,d.getOrDefault(a1,0)+1);
+*/

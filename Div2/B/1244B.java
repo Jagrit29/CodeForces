5e0d89dee26cc6b46428d888c84d86d2d543d86d
Jagrit <44747746+Jagrit29@users.noreply.github.com>
2020-10-10 17:33:38 +0530

    Create 1244B.java

---
+++ Div2/B/1244B.java
@@ -0,0 +1,133 @@
+import java.util.*;
+
+
+
+import java.math.*;
+import java.io.*;
+import java.lang.*;
+public class Main {
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+ 
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+ 
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (final IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (final IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+		int[] nextIntArray(final int n) {
+			final int a[] = new int[n];
+			for (int i = 0; i < n; i++) {
+				a[i] = Integer.parseInt(next());
+			}
+			return a;
+		}
+
+	}
+
+	private static long startTime = System.currentTimeMillis();
+
+	public static void main(final String[] args) {
+		FastReader sc =new FastReader();
+		int t=sc.nextInt();
+		while(t-- > 0 )
+		{
+			int n=sc.nextInt();
+			String s=sc.next();
+			int max=n;
+			for(int i=0;i<n;i++)
+			{
+				if(s.charAt(i)=='1')
+				{
+					int possible=(i+1)*2;
+					int possible2=(n-i)*2;
+					max=Math.max(possible,max);
+					max=Math.max(possible2,max);
+				}
+			}
+			System.out.println(max);
+		}
+
+
+
+	}
+	
+}
+
+
+//Templates for Comparator and Classes - @jagrit_07
+/*
+Arrays.sort(newEmployees, new Comparator<Employee>() {
+    @Override
+    public int compare(Employee emp1, Employee emp2) {
+        return emp1.getName().compareTo(emp2.getName());
+    }
+});
+class Pair
+	{
+		
+		long i; //index;
+		long l; //left;
+		long c; //cost;
+		public Pair(long x,long y,long z)
+		{
+			this.i=x;
+			this.l=y;
+			this.c=z;
+		}
+		public String toString()
+		{
+			return this.i+" "+this.l+" "+this.c;
+		}
+	}
+class Comp implements Comparator<Pair>
+	{
+		public int compare(Pair p1, Pair p2)
+		{
+			if(p1.c!=p2.c)
+			{
+				return (int)(p1.c-p2.c); //sort acc to cost;
+			}
+			else{
+				return (int)(p1.i-p2.i); //sort acc to index;
+			}
+			
+		}
+	}
+*/
+
+/*
+  HashMap - Put template - d.put(a1,d.getOrDefault(a1,0)+1);
+*/

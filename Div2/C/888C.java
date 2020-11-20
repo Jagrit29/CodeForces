0e0d2c33362eb37404309c72557ada940de46b7b
Jagrit <44747746+Jagrit29@users.noreply.github.com>
2020-11-21 02:45:59 +0530

    Create 888C.java

---
+++ Div2/C/888C.java
@@ -0,0 +1,185 @@
+import java.util.*;
+import java.math.*;
+import java.io.*;
+import java.lang.*;
+
+public class Main {
+    static class Pair
+    {
+        int x;
+        int y;
+        public Pair(int x, int  y)
+        {
+            this.x=x;
+            this.y=y;
+        }
+    }
+static class Comp implements Comparator<Pair>
+    {
+        public int compare(Pair p1, Pair p2)
+        {
+            return p1.y-p2.y;
+        }
+    }
+    public static long dp1[][];
+	public static void main(String[] args) {
+		FastReader sc =new FastReader();
+        String s=sc.next();
+        int last[]=new int[26];
+        //Intially all are at 0;
+        //K will be max for each char;
+        //The difference between the occurence of each charater;
+        int k[]=new int[26];
+        for(int i=1;i<=s.length();i++)
+        {
+            //I will follow 1 based indexing;
+            char c=s.charAt(i-1); //this is the char at length i;
+            int prev=last[c-'a']; //Previously it came at this;
+            int newv=i; //its coming at this;
+            k[c-'a']=Math.max(k[c-'a'],newv-prev);
+            last[c-'a']=i;
+        }
+        
+        //Now each will be ending at s.length(); as well;
+        for(int i=0;i<26;i++)
+        {
+            k[i]=Math.max(k[i],s.length()-last[i]+1); //considering case ab;
+        }
+        
+        int ans=Integer.MAX_VALUE;
+        for(int i=0;i<26;i++)
+        {
+            if(k[i]!=0) ans=Math.min(k[i],ans);
+        }
+        System.out.println(ans);
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    static int gcd(int a,int b)
+    {
+        if(b==0) return a;
+        return gcd(b,a%b);
+    }
+    
+    
+    static void sort(int[] a) {
+		ArrayList<Integer> l=new ArrayList<>();
+		for (int i:a) l.add(i);
+		Collections.sort(l);
+		for (int i=0; i<a.length; i++) a[i]=l.get(i);
+	}
+    static void sort(long[] a) {
+		ArrayList<Long> l=new ArrayList<>();
+		for (long i:a) l.add(i);
+		Collections.sort(l);
+		for (int i=0; i<a.length; i++) a[i]=l.get(i);
+	}
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+ 
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+ 
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (final IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (final IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+		int[] nextIntArray(final int n) {
+			final int a[] = new int[n];
+			for (int i = 0; i < n; i++) {
+				a[i] = Integer.parseInt(next());
+			}
+			return a;
+		}
+
+	}	
+}
+
+
+//Templates for Comparator and Classes - @jagrit_07
+/*
+Arrays.sort(newEmployees, new Comparator<Employee>() {
+    @Override
+    public int compare(Employee emp1, Employee emp2) {
+        return emp1.getName().compareTo(emp2.getName());
+    }
+});
+class Pair
+	{
+		
+		long i; //index;
+		long l; //left;
+		long c; //cost;
+		public Pair(long x,long y,long z)
+		{
+			this.i=x;
+			this.l=y;
+			this.c=z;
+		}
+		public String toString()
+		{
+			return this.i+" "+this.l+" "+this.c;
+		}
+	}
+class Comp implements Comparator<Pair>
+	{
+		public int compare(Pair p1, Pair p2)
+		{
+			if(p1.c!=p2.c)
+			{
+				return (int)(p1.c-p2.c); //sort acc to cost;
+			}
+			else{
+				return (int)(p1.i-p2.i); //sort acc to index;
+			}
+			
+		}
+	}
+*/
+
+/*
+  HashMap - Put template - d.put(a1,d.getOrDefault(a1,0)+1);
+  for (Map.Entry<String, Integer> entry : map.entrySet()) {
+        System.out.println(entry.getKey() + ":" + entry.getValue());
+    }
+*/

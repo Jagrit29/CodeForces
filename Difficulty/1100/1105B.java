49a5eb9ce68b31f259d949fe965943ea53c03a8d
Jagrit <44747746+Jagrit29@users.noreply.github.com>
2020-11-25 12:18:59 +0530

    Create 1105B.java

---
+++ Difficulty/1100/1105B.java
@@ -0,0 +1,231 @@
+import java.util.*;
+import java.math.*;
+import java.io.*;
+import java.lang.*;
+
+public class Main {
+	public static void main(String[] args) {
+        FastReader sc = new FastReader();
+        int n=sc.nextInt();
+        int k=sc.nextInt();
+        List<Integer> a[] = new ArrayList[26];
+        for(int i=0;i<a.length;i++)
+        {
+            a[i]=new ArrayList<>();
+        }
+        String s=sc.next();
+        int count=1;
+        if(s.length()==1)
+        {
+            a[s.charAt(0)-'a'].add(1);
+        }
+        for(int i=1;i<n;i++)
+        {
+            if(s.charAt(i)==s.charAt(i-1))
+            {
+                count++;
+                if(i==n-1)
+                {
+                    a[s.charAt(i)-'a'].add(count);
+                }
+            }
+            else
+            {
+                if(i==n-1)
+                {
+                    a[s.charAt(i)-'a'].add(1);
+                }
+                a[s.charAt(i-1)-'a'].add(count);
+                count=1;
+            }
+        }
+        int ans=0;
+        for(int i=0;i<26;i++)
+        {
+            int count2=0;
+            //System.out.println(Arrays.toString(a[i].toArray()));
+            for(int li: a[i])
+            {
+                if(li>=k)
+                {
+                    //that many substrings from this;
+                    count2=count2 + (li/k);
+                }
+            }
+            ans=Math.max(ans,count2);
+        }
+        System.out.println(ans);
+        
+        
+		
+    }
+    public static int partition(int arr[], int low, int high) 
+    { 
+        int pivot = arr[high];  
+        int i = (low-1);
+        for (int j=low; j<high; j++) 
+        { 
+            if (arr[j] < pivot) 
+            { 
+                i++; 
+                int temp = arr[i]; 
+                arr[i] = arr[j]; 
+                arr[j] = temp; 
+            } 
+        } 
+        int temp = arr[i+1]; 
+        arr[i+1] = arr[high]; 
+        arr[high] = temp; 
+  
+        return i+1; 
+    } 
+    public static void sort(int arr[], int low, int high) 
+    { 
+        if (low < high) 
+        { 
+            int pi = partition(arr, low, high); 
+            sort(arr, low, pi-1); 
+            sort(arr, pi+1, high); 
+        } 
+    } 
+    static class Pair
+    {
+        int x;
+        int y;
+        public Pair(int x, int  y)
+        {
+            this.x=x;
+            this.y=y;
+        }
+    }
+static class Comp implements Comparator<Pair>
+    {
+        public int compare(Pair p1, Pair p2)
+        {
+            return p1.y-p2.y;
+        }
+    }
+    
+    
+    
+    
+    
+    static int gcd(int a,int b)
+    {
+        if(b==0) return a;
+        return gcd(b,a%b);
+    }
+    
+    
+    static void sort(int[] a) {
+		ArrayList<Integer> l=new ArrayList<>();
+		for (int i:a) l.add(i);
+		Collections.sort(l);
+		for (int i=0; i<a.length; i++) a[i]=l.get(i);
+	}
+    static void sort(long[] a) {
+		ArrayList<Long> l=new ArrayList<>();
+		for (long i:a) l.add(i);
+		Collections.sort(l);
+		for (int i=0; i<a.length; i++) a[i]=l.get(i);
+	}
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+ 
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+ 
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (final IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (final IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+		int[] nextIntArray(final int n) {
+			final int a[] = new int[n];
+			for (int i = 0; i < n; i++) {
+				a[i] = Integer.parseInt(next());
+			}
+			return a;
+		}
+
+	}	
+}
+
+
+//Templates for Comparator and Classes - @jagrit_07
+/*
+Arrays.sort(newEmployees, new Comparator<Employee>() {
+    @Override
+    public int compare(Employee emp1, Employee emp2) {
+        return emp1.getName().compareTo(emp2.getName());
+    }
+});
+class Pair
+	{
+		
+		long i; //index;
+		long l; //left;
+		long c; //cost;
+		public Pair(long x,long y,long z)
+		{
+			this.i=x;
+			this.l=y;
+			this.c=z;
+		}
+		public String toString()
+		{
+			return this.i+" "+this.l+" "+this.c;
+		}
+	}
+class Comp implements Comparator<Pair>
+	{
+		public int compare(Pair p1, Pair p2)
+		{
+			if(p1.c!=p2.c)
+			{
+				return (int)(p1.c-p2.c); //sort acc to cost;
+			}
+			else{
+				return (int)(p1.i-p2.i); //sort acc to index;
+			}
+			
+		}
+	}
+*/
+
+/*
+  HashMap - Put template - d.put(a1,d.getOrDefault(a1,0)+1);
+  for (Map.Entry<String, Integer> entry : map.entrySet()) {
+        System.out.println(entry.getKey() + ":" + entry.getValue());
+    }
+*/

167f5ac6be89edd09ca6359c6958bdc5ad7e8d10
Jagrit <44747746+Jagrit29@users.noreply.github.com>
2020-12-05 22:36:35 +0530

    Create 459B.java

---
+++ Difficulty/1300/459B.java
@@ -0,0 +1,228 @@
+import java.util.*;
+import java.math.*;
+import java.io.*;
+import java.lang.*;
+
+public class Main {
+    
+	public static void main(String[] args) {
+        FastReader sc =new FastReader();
+        int n = sc.nextInt();
+        int a[] = sc.nextIntArray(n);
+        sort(a);
+        if(a[0]==a[n-1])
+        {
+            long nn=n;
+            System.out.println(0+" "+(nn*(nn-1))/2);
+        }
+        else
+        {
+            long c1=1;
+            int num=a[0];
+            int i=1;
+            while(i<n && a[i]==num)
+            {
+                i++;
+                c1++;
+            }
+            int j=n-2;
+            long c2=1;
+            int num2=a[n-1];
+            while(j>=0 && a[j]==num2)
+            {
+                c2++;
+                j--;
+            }
+            System.out.println(a[n-1]-a[0] +" "+(c1*c2));
+        }
+        
+        
+       
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    static class Pair
+    {
+        int x;
+        int y;
+        public Pair(int x, int  y)
+        {
+            this.x=x;
+            this.y=y;
+        }
+    }
+    static class Comp implements Comparator<Pair>
+    {
+        public int compare(Pair p1, Pair p2)
+        {
+            if(p1.x!=p2.x && p1.y!=p2.y)
+            {
+                return p1.x-p2.x;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+    public static int binarySearch(int num[], int x)
+    {
+        int low=0;
+        int high=num.length-1;
+        int ans=-1;
+        while(low<=high)
+        {
+            int mid = low + (high-low)/2;
+            if(num[mid]==x)
+            {
+                return mid;
+            }
+            else if(num[mid]<x)
+            {
+                low=mid+1;
+            }
+            else
+            {
+                ans=mid;
+                high=mid-1;
+            }
+        }
+        return ans;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    static int gcd(int a,int b)
+    {
+        if(b==0) return a;
+        return gcd(b,a%b);
+    }
+    
+    
+    static void sort(int[] a) {
+		ArrayList<Integer> l=new ArrayList<>();
+		for (int i:a) l.add(i);
+		Collections.sort(l);
+		for (int i=0; i<a.length; i++) a[i]=l.get(i);
+	}
+    static void sort(long[] a) {
+		ArrayList<Long> l=new ArrayList<>();
+		for (long i:a) l.add(i);
+		Collections.sort(l);
+		for (int i=0; i<a.length; i++) a[i]=l.get(i);
+	}
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+ 
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+ 
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (final IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (final IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+		int[] nextIntArray(final int n) {
+			final int a[] = new int[n];
+			for (int i = 0; i < n; i++) {
+				a[i] = Integer.parseInt(next());
+			}
+			return a;
+		}
+
+	}	
+}
+//private ArrayList<String[]> action = new ArrayList<String[]>();
+
+
+//Templates for Comparator and Classes - @jagrit_07
+/*
+Arrays.sort(newEmployees, new Comparator<Employee>() {
+    @Override
+    public int compare(Employee emp1, Employee emp2) {
+        return emp1.getName().compareTo(emp2.getName());
+    }
+});
+class Pair
+	{
+		
+		long i; //index;
+		long l; //left;
+		long c; //cost;
+		public Pair(long x,long y,long z)
+		{
+			this.i=x;
+			this.l=y;
+			this.c=z;
+		}
+		public String toString()
+		{
+			return this.i+" "+this.l+" "+this.c;
+		}
+	}
+class Comp implements Comparator<Pair>
+	{
+		public int compare(Pair p1, Pair p2)
+		{
+			if(p1.c!=p2.c)
+			{
+				return (int)(p1.c-p2.c); //sort acc to cost;
+			}
+			else{
+				return (int)(p1.i-p2.i); //sort acc to index;
+			}
+			
+		}
+	}
+*/
+
+/*
+  HashMap - Put template - d.put(a1,d.getOrDefault(a1,0)+1);
+  for (Map.Entry<String, Integer> entry : map.entrySet()) {
+        System.out.println(entry.getKey() + ":" + entry.getValue());
+    }
+*/

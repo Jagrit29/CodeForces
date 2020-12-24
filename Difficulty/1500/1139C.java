13d96b76f0d3df54b9358b7205c12279265003e7
Jagrit <44747746+Jagrit29@users.noreply.github.com>
2020-12-24 17:45:40 +0530

    Create 1139C.java

---
+++ Difficulty/1500/1139C.java
@@ -0,0 +1,339 @@
+import java.util.*;
+import java.math.*;
+import java.io.*;
+import java.lang.*;
+
+public class Main {
+    //public static List<Integer> list = new ArrayList<>();
+	public static void main(String[] args) {
+        FastReader sc = new FastReader();
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        List<List<Integer>> graph = new ArrayList<>();
+        for(int i=0;i<n;i++)
+        {
+            graph.add(new ArrayList<>());
+            graph.get(i).add(i); //self loop in each;
+        }
+        int mod = (int)Math.pow(10,9)+7;
+        long total = (long)power(n,k,mod);  //n^k % mod;
+        //System.out.println(total);
+        //This graph will only contain edges with all red colors;
+        for(int i=0;i<n-1;i++)
+        {
+            int u = sc.nextInt()-1;
+            int v = sc.nextInt()-1;
+            int c = sc.nextInt();
+            if(c==1) continue;
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+        
+        //AtleastOneBlack = Total - allRed; 
+        
+        long allRed = 0;
+        HashSet<Integer> visited = new HashSet<>();
+        for(int i=0;i<n;i++)
+        {
+            if(visited.contains(i)==false)
+            {
+                int sizeOfGroup = dfs(graph,i,visited,0);
+                //System.out.println(sizeOfGroup);
+                allRed = (allRed + power(sizeOfGroup,k,mod))%mod;
+            }
+        }  
+        System.out.println((total - allRed + mod)%mod);
+    }
+    /*
+    Why I need to add mod;
+    If you do (num%mod), then the value will be in the range of 0 to mod-1.
+Hence if you got a negative number somehow, then you have to add mod into our result.
+Because, (num+mod)%mod = ((num%mod)+(mod%mod))%mod = (num%mod+0)%mod = num%mod.
+Which will be positive. Hence we have to add the mod in negative number until it becomes positive.
+    
+    
+    */
+    public static int dfs(List<List<Integer>> graph, int i, HashSet<Integer> visited,int size)
+    {
+        if(visited.contains(i)) return 0;
+        visited.add(i);
+        size++;
+        List<Integer> neigh = graph.get(i);
+        for(int ne : neigh)
+        {
+            if(visited.contains(ne)==false)
+            {
+                size = dfs(graph,ne,visited,size);
+            }
+        }
+        return size;
+    }
+    
+    
+    /* Iterative Function to calculate (x^y)%p in O(log y) */
+    static long power(long x, long y, int p) 
+    { 
+        // Initialize result 
+        long ans = 1;      
+         
+        // Update x if it is more   
+        // than or equal to p 
+        x = x % p;  
+  
+       if (x == 0) return 0; // In case x is divisible by p;    
+  
+        while (y > 0) 
+        { 
+            if((y & 1)==1) 
+            {
+                 ans = ((long)(ans * x))% p;    
+            }
+            y = y >> 1;  //y/2; 
+            x = ((long)(x * x) )% p;  
+        } 
+        return ans; 
+    } 
+    
+    
+    
+    
+    
+    public static void printList(List<Integer> list)
+    {
+        StringBuffer bf = new StringBuffer("");
+        for(int i: list) bf.append(i+" ");
+        System.out.println(bf.toString());
+    }
+    
+    
+    
+    
+    
+    public static int sum(int a[])
+    {
+        int sum=0;
+        for(int i: a) sum=sum+i;
+        return sum;
+    }
+    
+  
+    public static int getMax(int a[])
+    {
+        int m=Integer.MIN_VALUE;
+        for(int i: a) m=Math.max(i,m);
+        return m;
+    }
+    
+    static class Pair
+    {
+        int x;
+        int y;
+
+        //int i;
+        public Pair(int x, int  y)
+        {
+            this.x=x;
+            this.y=y;
+        }
+    }
+    static class Comp implements Comparator<Pair>
+    {
+        public int compare(Pair p1, Pair p2)
+        {
+            if(p1.y!=p2.y)
+            {
+                return p2.y - p1.y;
+            }
+            return p2.x - p1.x;
+        }
+    }
+    public static int bs(long num[], long x)
+    {
+        int low=0;
+        int high=num.length-1;
+        int ans=-1;
+        while(low<=high)
+        {
+            int mid = low + (high-low)/2;
+            if(num[mid]==x)
+            {
+                ans = mid;
+                high=mid-1;
+            }
+            else if(num[mid]<x)
+            {
+               // ans=mid;
+                low=mid+1;
+            }
+            else
+            {
+                ans=mid;
+                high=mid-1;
+            }
+        }
+        return ans;
+    }
+    
+    
+    public static int partition(int arr[], int low, int high) 
+    { 
+        int pivot = arr[high];  
+        int i = (low-1);
+        for (int j=low; j<high; j++) 
+        { 
+            if (arr[j] < pivot) 
+            { 
+                i++; 
+                int temp = arr[i]; 
+                arr[i] = arr[j]; 
+                arr[j] = temp; 
+            } 
+        } 
+        int temp = arr[i+1]; 
+        arr[i+1] = arr[high]; 
+        arr[high] = temp; 
+  
+        return i+1; 
+    } 
+    public static void sort(int arr[], int low, int high) 
+    { 
+        if (low < high) 
+        { 
+            int pi = partition(arr, low, high); 
+            sort(arr, low, pi-1); 
+            sort(arr, pi+1, high); 
+        } 
+    } 
+    
+    
+    
+    
+    
+    
+    static long gcd(long a,long b)
+    {
+        if(b==0) return a;
+        return gcd(b,a%b);
+    }
+    
+    
+    static void sort(int[] a) {
+		ArrayList<Integer> l=new ArrayList<>();
+		for (int i:a) l.add(i);
+		Collections.sort(l);
+		for (int i=0; i<a.length; i++) a[i]=l.get(i);
+	}
+    static void sort(long[] a) {
+		ArrayList<Long> l=new ArrayList<>();
+		for (long i:a) l.add(i);
+		Collections.sort(l);
+		for (int i=0; i<a.length; i++) a[i]=l.get(i);
+	}
+    
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+ 
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+ 
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (final IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (final IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+		int[] nextIntArray(final int n) {
+			final int a[] = new int[n];
+			for (int i = 0; i < n; i++) {
+				a[i] = Integer.parseInt(next());
+			}
+			return a;
+		}
+
+	}	
+}
+//private ArrayList<String[]> action = new ArrayList<String[]>();
+
+
+//Templates for Comparator and Classes - @jagrit_07
+/*
+Arrays.sort(newEmployees, new Comparator<Employee>() {
+    @Override
+    public int compare(Employee emp1, Employee emp2) {
+        return emp1.getName().compareTo(emp2.getName());
+    }
+});
+class Pair
+	{
+		
+		long i; //index;
+		long l; //left;
+		long c; //cost;
+		public Pair(long x,long y,long z)
+		{
+			this.i=x;
+			this.l=y;
+			this.c=z;
+		}
+		public String toString()
+		{
+			return this.i+" "+this.l+" "+this.c;
+		}
+	}
+class Comp implements Comparator<Pair>
+	{
+		public int compare(Pair p1, Pair p2)
+		{
+			if(p1.c!=p2.c)
+			{
+				return (int)(p1.c-p2.c); //sort acc to cost;
+			}
+			else{
+				return (int)(p1.i-p2.i); //sort acc to index;
+			}
+			
+		}
+	}
+*/
+
+/*
+System.out.println( 
+      String.format("%.5f", b)); 
+  HashMap - Put template - d.put(a1,d.getOrDefault(a1,0)+1);
+  for (Map.Entry<String, Integer> entry : map.entrySet()) {
+        System.out.println(entry.getKey() + ":" + entry.getValue());
+    }
+  Deque<String> deque 
+            = new LinkedList<String>();  
+            
+  List<Integer> c[] = new ArrayList[3];          
+*/
